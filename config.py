fdf33de94739e752b45f54fd0f229959bde387b8
Quirin Brändli <quirin.braendli@gmx.de>
2020-05-27 11:26:42 +0200

    Configs

--- config.py
+++ config.py
@@ -8,11 +8,25 @@ from string import Template
 
 
 def create_config():
-    config = open("db/config.ini", "w")
+    config = configparser.ConfigParser()
     config.add_section('Host')
     config.set('Host', 'Hostname', 'ldap://ldap.example.com')
-    config.write()
-    config.close()
+    config['Host']['BaseDN'] = 'OU=people,dc=example,dc=com'
+    config['Host']['DisplayName'] = 'Active Directory'
+    config['Bind'] = {}
+    config['Bind']['User'] = 'CN=admin,dc=example,dc=eu'
+    config['Bind']['Password'] = 'CN=admin,dc=example,dc=eu'
+    config['LDAP params'] = {}
+    config['LDAP params']['Username'] = 'userPrincipalName'
+    config['LDAP params']['Fullname'] = 'cn'
+    config['LDAP params']['Mail Address'] = 'mail'
+    config['LDAP params']['Active User'] = 'userAccountControl'
+    config['MailCow'] = {}
+    config['MailCow']['Hostname'] = 'https://mail.example.com'
+    config['MailCow']['API-Key'] = 'XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX'
+    config['MailCow']['Sync-Interval'] = 300
+    with open('db/config.ini', 'w') as configfile:
+        config.write(configfile)
 
 def apply_config(config_file, config_data):
     if os.path.isfile(config_file):
